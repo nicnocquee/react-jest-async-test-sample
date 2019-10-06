28158537f433d02dcd01ad374570801e5d9151ad
nico <nico@delightfuldev.com>
2019-10-06 18:42:48 +0200

    test fail

--- src/app.js
+++ src/app.js
@@ -24,7 +24,6 @@ const App = () => {
   return (
     <div>
       {isLoading && <p data-testid="loading">Loading ...</p>}
-      {error && <p data-testid="error">Error</p>}
       {data && <p data-testid="data">{JSON.stringify(data, null, 2)}</p>}
       {!isLoading && (
         <button data-testid="check" onClick={() => setLastChecked(Date.now())}>
